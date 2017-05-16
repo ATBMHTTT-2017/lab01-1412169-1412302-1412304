3fffdc7b286c8093d5fc8540dd6e1f89539992a0
congloi1996 <congloi1996@gmail.com>
2017-05-16 15:36:23 +0700

    Tạo tài khoản cho user
    Tạo tài khoản cho các nhân viên trong bảng Nhân Viên. Tên tài khoản trùng với mã nhân viên. Tạo ít nhất 5 tài khoản cho mỗi vị trí: trưởng dự án, trưởng phòng, trưởng chi nhánh, nhân viên, giám đốc và ít nhất 5 dòng cho các bảng dữ liệu còn lại

---
+++ TaoTaiKhoan_1412302.sql
@@ -0,0 +1,52 @@
+--USER CNDN
+create user NV000 identified by 123;
+create user NV001 identified by 123;
+create user NV011 identified by 123;
+create user NV012 identified by 123;
+create user NV021 identified by 123;
+create user NV022 identified by 123;
+create user NV031 identified by 123;
+create user NV041 identified by 123;
+create user NV042 identified by 123;
+create user NV043 identified by 123;
+
+--USER CNHCM
+create user NV101 identified by 123;
+create user NV111 identified by 123;
+create user NV112 identified by 123;
+create user NV113 identified by 123;
+create user NV121 identified by 123;
+create user NV122 identified by 123;
+create user NV131 identified by 123;
+create user NV141 identified by 123;
+create user NV142 identified by 123;
+create user NV143 identified by 123;
+
+--USER CNHN
+create user NV201 identified by 123;
+create user NV211 identified by 123;
+create user NV212 identified by 123;
+create user NV213 identified by 123;
+create user NV221 identified by 123;
+create user NV231 identified by 123;
+create user NV232 identified by 123;
+create user NV241 identified by 123;
+create user NV242 identified by 123;
+create user NV243 identified by 123;
+
+--USER CNHP
+create user NV301 identified by 123;
+create user NV311 identified by 123;
+create user NV321 identified by 123;
+create user NV331 identified by 123;
+create user NV332 identified by 123;
+create user NV333 identified by 123;
+
+--USER CNQN
+create user NV401 identified by 123;
+create user NV411 identified by 123;
+create user NV421 identified by 123;
+create user NV422 identified by 123;
+create user NV431 identified by 123;
+
+
